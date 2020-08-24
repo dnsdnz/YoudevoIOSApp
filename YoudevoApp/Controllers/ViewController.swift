93674c7d84158a2308cc854db291760825fb3980
Deniz MacBook Air <macbookair@MacBook-MacBook-Air.local>
2020-08-24 21:07:55 +0300

    new request functions created

--- YoudevoApp/Controllers/ViewController.swift
+++ YoudevoApp/Controllers/ViewController.swift
@@ -12,20 +12,143 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var lblTest: UILabel!
+    
     var testVar = ""
+    var semaphore = DispatchSemaphore (value: 0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        func getdata(){
-               let url = "http://www.haberler.youdevo.com/api/read.php"
- 
-         
-           }
-       
     }
+           func postData(){
+            
+            
+            let parameters = "{\n    \"title\": \"deneme 9 \",\n    \"category\": \"deneme 9\",\n    \"content\": \"deneme 9\",\n    \"pic_name\": \"/Users/macbookair/Desktop/photo/DSCN0343.png\"\n}"
+            let postData = parameters.data(using: .utf8)
+
+            var request = URLRequest(url: URL(string: "http://www.haberler.youdevo.com/api/insert.php")!,timeoutInterval: Double.infinity)
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+            request.httpMethod = "POST"
+            request.httpBody = postData
+
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+              guard let data = data else {
+                print(String(describing: error))
+                return
+              }
+              print(String(data: data, encoding: .utf8)!)
+                self.semaphore.signal()
+            }
+
+            task.resume()
+            semaphore.wait()
+
+          }
+            
+
+    func getData(){
+              
+
+        var request = URLRequest(url: URL(string: "http://www.haberler.youdevo.com/api/read.php")!,timeoutInterval: Double.infinity)
+        request.httpMethod = "GET"
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+          guard let data = data else {
+            print(String(describing: error))
+            return
+          }
+          print(String(data: data, encoding: .utf8)!)
+            self.semaphore.signal()
+        }
+
+        task.resume()
+        semaphore.wait()
+
+           }
+    
+    func getDataWithId(){
+             
+           let parameters = "{\n    \"news_id\":33\n}"
+           let postData = parameters.data(using: .utf8)
+
+           var request = URLRequest(url: URL(string: "http://www.haberler.youdevo.com/api/read.php?news_id=80")!,timeoutInterval: Double.infinity)
+           request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+           request.httpMethod = "GET"
+           request.httpBody = postData
+
+           let task = URLSession.shared.dataTask(with: request) { data, response, error in
+             guard let data = data else {
+               print(String(describing: error))
+               return
+             }
+             print(String(data: data, encoding: .utf8)!)
+            self.semaphore.signal()
+           }
+
+           task.resume()
+           semaphore.wait()
 
 
+          }
+       
+    
+    func putData(){
+        
+       let parameters = "{\n    \"news_id\":80,\n    \"title\":\"update test\"\n}"
+       let postData = parameters.data(using: .utf8)
+
+       var request = URLRequest(url: URL(string: "http://www.haberler.youdevo.com/api/update.php")!,timeoutInterval: Double.infinity)
+       request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+       request.httpMethod = "PUT"
+       request.httpBody = postData
+
+       let task = URLSession.shared.dataTask(with: request) { data, response, error in
+         guard let data = data else {
+           print(String(describing: error))
+           return
+         }
+         print(String(data: data, encoding: .utf8)!)
+        self.semaphore.signal()
+       }
+
+       task.resume()
+       semaphore.wait()
+
+
+              
+           }
+    
+   
+    func deleteData(){
+        
+               
+             let parameters = "{\n    \"news_id\":33\n}"
+             let postData = parameters.data(using: .utf8)
+
+             var request = URLRequest(url: URL(string: "http://www.haberler.youdevo.com/api/delete.php")!,timeoutInterval: Double.infinity)
+             request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+             request.httpMethod = "DELETE"
+             request.httpBody = postData
+
+             let task = URLSession.shared.dataTask(with: request) { data, response, error in
+               guard let data = data else {
+                 print(String(describing: error))
+                 return
+               }
+               print(String(data: data, encoding: .utf8)!)
+                self.semaphore.signal()
+             }
+
+             task.resume()
+             semaphore.wait()
+
+           
+           }
+
+   
 }
-
